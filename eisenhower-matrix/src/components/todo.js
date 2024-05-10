f518c9e3d2692f65efc9d25b7b09300c1566e907
Jonathan Andujo Flores <jonathana@microsoft.com>
2024-05-10 10:09:54 -0600

    Adding features

--- eisenhower-matrix/src/components/todo.js
+++ eisenhower-matrix/src/components/todo.js
@@ -8,14 +8,15 @@ const TodoList = (props) => {
         const newTask = {
             id: crypto.randomUUID(),
             todo: inputTask
+            //order: Math.max(...list.map(e=> e.order ?? 0),0)+1
         };
         setList([...list, newTask]);
         setInputTask('');
     };
 
     useEffect(() => {
-          saveInDb();
-      }, [list]);
+        saveInDb();
+    }, [list]);
 
     const handleDeleteTodo = (id) => {
         const newList = list.filter((todo) => todo.id !== id);
@@ -24,10 +25,34 @@ const TodoList = (props) => {
 
     const handleInputChange = (event) => {
         setInputTask(event.target.value);
+        console.log(event);
     };
 
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter')
+            handleAddTodo();
+    }
+
     const saveInDb = () => {
-        window.localStorage.setItem('dbPriority' + props.priority,JSON.stringify(list));
+        window.localStorage.setItem('dbPriority' + props.priority, JSON.stringify(list));
+    }
+
+    const moveUp = (index) => {
+        if (index > 0) {
+            const updatedList = [...list];
+            [updatedList[index], updatedList[index - 1]] =
+                [updatedList[index - 1], updatedList[index]];
+            setList(updatedList);
+        }
+    }
+
+    const moveDown = (index) => {
+        if (index < list.length - 1) {
+            const updatedList = [...list];
+            [updatedList[index], updatedList[index + 1]] =
+                [updatedList[index + 1], updatedList[index]];
+            setList(updatedList);
+        }
     }
 
     return (
@@ -36,18 +61,28 @@ const TodoList = (props) => {
 
             <div className="Top">
                 <input className="input" type="text" value={inputTask}
-                    onChange={handleInputChange} placeholder="Name of your task" />
+                    onChange={handleInputChange} onKeyDown={handleKeyDown} placeholder="Name of your task" />
 
-                <button className="btn" onClick={handleAddTodo}>ADD</button>
+                <button className="btn" onClick={handleAddTodo}>➕</button>
             </div>
 
             <ul>
-                {list.map((todo) => (
+                {list.map((todo, index) => (
                     <li className="task" key={todo.id}>
+                        <strong className='taskText'>
                         {todo.todo}
-                        <text className='deleteButton' onClick={() => handleDeleteTodo(todo.id)}>
-                            X
-                        </text>
+                        </strong>
+                        {index > 0 && <button onClick={() => moveUp(index)}>
+                            ⬆
+                        </button>
+                        }
+                        {index < list.length -1 && <button onClick={() => moveDown(index)}>
+                            ⬇
+                        </button>
+                        }
+                        <button onClick={() => handleDeleteTodo(todo.id)}>
+                            ❌
+                        </button>
                     </li>
                 ))}
             </ul>
